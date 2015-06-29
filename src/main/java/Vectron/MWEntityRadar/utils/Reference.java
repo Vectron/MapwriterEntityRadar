66219af0e3f5a5532119a685e4ced45faa98e389
Vectron <thijs_bloebaum@hotmail.com>
2015-06-29 15:49:27 +0200

    make mapwriter not requiered
    this makes it posible to install the radar on server only.
    client side the game will crash when mapwriter is not installed

--- src/main/java/Vectron/MWEntityRadar/utils/Reference.java
+++ src/main/java/Vectron/MWEntityRadar/utils/Reference.java
@@ -1,16 +1,14 @@
 package Vectron.MWEntityRadar.utils;
 
-import java.util.regex.Pattern;
-
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraft.util.ResourceLocation;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
 public class Reference 
 {
 	public static final String MOD_ID = "mwentityradar";
 	public static final String MOD_NAME = "Map Writer Entity Radar";
 	public static final String VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES =  "required-after:MapWriter";
+    public static final String DEPENDENCIES =  "after:MapWriter";
 	public static final String MOD_GUIFACTORY_CLASS = "mapwriter.gui.ModGuiFactoryHandler";
 	
 	public static final String CLIENT_PROXY_CLASS = "Vectron.MWEntityRadar.Proxy.ProxyClient";
