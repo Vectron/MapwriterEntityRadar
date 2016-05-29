8a7073430af1d445e41df8c203cb79003f57dcc7
Vectron <Vectron@Vectrons-PC>
2016-05-29 17:09:04 +0200

    removed api, and just reference the dev version, some minor updates to make it a release version

--- src/api/java/mapwriter/api/ILabelInfo.java
+++
@@ -1,8 +0,0 @@
-package mapwriter.api;
-
-public interface ILabelInfo
-{
-	// StringArray of text to render in the hover label, each position in the
-	// array is a newline
-	public String[] getInfoText();
-}
--- src/api/java/mapwriter/api/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(owner = "MapWriter", provides = "MapWriterApi", apiVersion = "1.0.0")
-package mapwriter.api;
-
-import net.minecraftforge.fml.common.API;
-
